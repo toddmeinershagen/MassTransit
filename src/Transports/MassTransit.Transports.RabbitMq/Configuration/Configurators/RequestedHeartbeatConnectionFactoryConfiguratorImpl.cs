85a105d84ef4ddbb8fdd2042d9534eee070aa844
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-06-18 15:45:29 +0200

    Added ability to set RabbitMQ.Client.ConnectionFactory.RequestedHeartbeat

---
+++ src/Transports/MassTransit.Transports.RabbitMq/Configuration/Configurators/RequestedHeartbeatConnectionFactoryConfiguratorImpl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using MassTransit.Configurators;
+using MassTransit.Transports.RabbitMq.Configuration.Builders;
+using RabbitMQ.Client;
+
+namespace MassTransit.Transports.RabbitMq.Configuration.Configurators
+{
+	public class RequestedHeartbeatConnectionFactoryConfiguratorImpl : ConnectionFactoryBuilderConfigurator
+	{
+		private readonly ushort _requestedHeartbeat;
+
+		public RequestedHeartbeatConnectionFactoryConfiguratorImpl(ushort requestedHeartbeat)
+		{
+			_requestedHeartbeat = requestedHeartbeat;
+		}
+
+		public IEnumerable<ValidationResult> Validate()
+		{
+			return new ValidationResult[0];
+		}
+
+		public ConnectionFactoryBuilder Configure(ConnectionFactoryBuilder builder)
+		{
+			builder.Add(Configure);
+			return builder;
+		}
+
+		private ConnectionFactory Configure(ConnectionFactory connectionFactory)
+		{
+			connectionFactory.RequestedHeartbeat = _requestedHeartbeat;
+			return connectionFactory;
+		}
+	}
+}
